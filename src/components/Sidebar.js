777d5846a2af2a152c70194eb91b65f4b03a2eb3
Mikaeil Eghbal <mikaeileghbal@yahoo.com>
2023-02-20 14:03:39 +0330

    Add sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -3,26 +3,43 @@ import HomeIcon from "@mui/icons-material/Home";
 import VideocamIcon from "@mui/icons-material/Videocam";
 import LiveTvIcon from "@mui/icons-material/LiveTv";
 import SearchIcon from "@mui/icons-material/Search";
+import Link from "@mui/material/Link";
+import AppBar from "@mui/material/AppBar";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import { ListItemButton } from "@mui/material";
 
 export default function Sidebar() {
   return (
-    <div>
+    <AppBar
+      position="fixed"
+      color="secondary"
+      style={{ left: "0", width: "150px", height: "100vh" }}
+    >
       <nav>
-        <ul>
-          <li>
-            <HomeIcon />
-          </li>
-          <li>
-            <VideocamIcon />
-          </li>
-          <li>
-            <LiveTvIcon />
-          </li>
-          <li>
-            <SearchIcon />
-          </li>
-        </ul>
+        <List>
+          <ListItem>
+            <ListItemButton component="a" href="#simple-list">
+              <HomeIcon />
+            </ListItemButton>
+          </ListItem>
+          <ListItem>
+            <ListItemButton component="a" href="#simple-list">
+              <VideocamIcon />
+            </ListItemButton>
+          </ListItem>
+          <ListItem>
+            <ListItemButton component="a" href="#simple-list">
+              <LiveTvIcon />
+            </ListItemButton>
+          </ListItem>
+          <ListItem>
+            <ListItemButton component="a" href="#simple-list">
+              <SearchIcon />
+            </ListItemButton>
+          </ListItem>
+        </List>
       </nav>
-    </div>
+    </AppBar>
   );
 }
