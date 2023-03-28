c527837d94fda1b7c5ec34e31f6754526f83d3c3
Mikaeil <mikaeileghbal@yahoo.com>
2023-03-28 21:08:22 +0430

    Complete infinite load

--- src/index.js
+++ src/index.js
@@ -10,10 +10,8 @@ import { ScrollToTop } from "./hooks/ScrollTop";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <Router>
-      <ScrollToTop />
-      <App />
-    </Router>
-  </React.StrictMode>
+  <Router>
+    <ScrollToTop />
+    <App />
+  </Router>
 );
--- src/pages/List.js
+++ src/pages/List.js
@@ -1,45 +1,66 @@
-import React, { useEffect, useMemo, useState } from "react";
+import React, {
+  useCallback,
+  useEffect,
+  useMemo,
+  useRef,
+  useState,
+} from "react";
 import { useLocation, useParams } from "react-router-dom";
 import ViewList from "../components/ViewList";
 import apiEndpoint from "../utils/apiEndpoints";
-import MovieCar from "../components/MovieCard";
+import MovieCard from "../components/MovieCard";
 import { Grid, Typography } from "@mui/material";
 import Loading from "../components/Loading";
 
 export default function List() {
   const [page, setPage] = useState(1);
   const [items, setItems] = useState([]);
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(false);
 
   const { category } = useParams();
   const location = useLocation();
 
   const type = location.pathname.split("/")[1];
   const { url, title } = apiEndpoint[type][category];
+
   const urlWithPage = useMemo(() => `${url}&page=${page}`, [url, page]);
 
-  const handlePage = () => {
-    console.log("page: ", page);
-    setPage((page) => page + 1);
-  };
-
   useEffect(() => {
-    setLoading(true);
     const getData = async () => {
-      try {
-        const res = await fetch(urlWithPage);
-        const result = await res.json();
-        setLoading(false);
-        setItems((state) => [...state, ...result.results]);
-      } catch (err) {
-        setLoading(false);
-        return err;
-      }
+      setLoading(true);
+      const res = await fetch(urlWithPage);
+      const result = await res.json();
+      setItems((state) => [...state, ...result.results]);
+      setLoading(false);
     };
     getData();
-  }, [urlWithPage]);
+  }, [page]);
 
-  console.log("items", items);
+  let bottomBoundryRef = useRef(null);
+
+  const scrollObserver = useCallback(
+    (node) => {
+      new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+          if (entry.intersectionRatio > 0) {
+            setPage((page) => page + 1);
+          }
+        });
+      }).observe(node);
+    },
+    [setPage]
+  );
+
+  useEffect(() => {
+    if (bottomBoundryRef.current) {
+      scrollObserver(bottomBoundryRef.current);
+    }
+  }, [bottomBoundryRef]);
+
+  console.log(loading);
+  console.log(items);
+  console.log(urlWithPage);
+
   return (
     <ViewList>
       <Typography
@@ -62,12 +83,13 @@ export default function List() {
       >
         {items?.map((item) => (
           <Grid item xs={12 / 5} key={item.id}>
-            <MovieCar item={item} />
+            <MovieCard item={item} />
           </Grid>
         ))}
       </Grid>
+      <div ref={bottomBoundryRef}></div>
       {loading && <Loading />}
-      <button onClick={handlePage}>Load more {page}</button>
+      {/* <button onClick={handlePage}>Load more {page}</button> */}
     </ViewList>
   );
 }
