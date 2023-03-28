204de6047338659326c2ca555586d6322a7e38fe
Mikaeil Eghbal <mikaeileghbal@yahoo.com>
2023-03-28 13:07:04 +0430

    Add loading component

--- src/components/ViewSelection.js
+++ src/components/ViewSelection.js
@@ -1,29 +1,15 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { Box } from "@mui/system";
 import Footer from "./Footer";
 import Header from "./Header";
+import useFeaturedMovie from "../hooks/useFeaturedMovie";
 
 export default function ViewSelection({ children }) {
-  const [movie, setMovie] = useState([]);
-
-  useEffect(() => {
-    const getMovies = async () => {
-      const res = await fetch(
-        "https://api.themoviedb.org/3/movie/500?api_key=3e35d5ea16674bcc971aee7ed10f0919"
-      );
-      const result = await res.json();
-
-      console.log(result);
-      setMovie(result);
-    };
-
-    getMovies();
-  }, []);
+  const { movie } = useFeaturedMovie();
 
   return (
     <Box
       component="main"
-      //pl={7}
       sx={{ marginLeft: { xs: 0, lg: "100px" }, position: "absolute" }}
     >
       <Header movie={movie} />
