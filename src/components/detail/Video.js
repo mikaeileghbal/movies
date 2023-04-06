347a7929982bef842e264134e263e8a1eed11fe2
Mikaeil Eghbal <mikaeileghbal@yahoo.com>
2023-04-06 13:38:59 +0430

    Add filter to videos on detail page

--- src/components/detail/Video.js
+++ src/components/detail/Video.js
@@ -1,96 +1,171 @@
-import { Box, Chip, Grid, IconButton, Typography } from "@mui/material";
-import React from "react";
+import {
+  Box,
+  Chip,
+  FormControl,
+  Grid,
+  IconButton,
+  InputLabel,
+  MenuItem,
+  Select,
+  Typography,
+} from "@mui/material";
+import React, { useEffect, useState } from "react";
 import PlayCircleFilledWhiteOutlinedIcon from "@mui/icons-material/PlayCircleFilledWhiteOutlined";
 import { Link, useParams } from "react-router-dom";
 import apiEndpoint from "../../utils/apiEndpoints";
 import useMovieVideos from "../../hooks/useMovieVideos";
 import Loading from "../Loading";
+import styled from "@emotion/styled";
+
+const StyledMenuItem = styled(MenuItem)(({ theme }) => ({
+  fontSize: "12px",
+}));
 
 export default function Video() {
+  const [filter, setFilter] = useState("all");
+  const [filteredVideos, setFilteredVideos] = useState([]);
+  const [videoCount, setVideoCount] = useState(0);
+
   const { type, id } = useParams();
 
   const routePath = apiEndpoint[type].video;
 
   routePath.url = routePath.url.replace("{_id}", id);
-  console.log(routePath);
 
   const { isLoading, videos } = useMovieVideos(routePath.url);
+
+  const handleChange = (e) => {
+    console.log(e.target.value);
+    setFilter(e.target.value);
+  };
+
+  useEffect(() => {
+    if (filter !== "all") {
+      setFilteredVideos(
+        videos.filter(
+          (item) => item.type.toLowerCase() === filter.toLowerCase()
+        )
+      );
+    } else {
+      setFilteredVideos(videos);
+    }
+    setVideoCount(filteredVideos.length);
+  }, [filter, videos, filteredVideos.length]);
 
   if (isLoading) return <Loading />;
 
   return (
-    <Grid
-      pr={6.5}
-      container
-      mb={6}
-      columnSpacing={2}
-      rowSpacing={5}
-      sx={{ backgroundColor: "transparent" }}
-    >
-      {videos?.map((video) => (
-        <Grid item xs={4}>
-          <Link
-            to={`https://youtube.com/watch?v=${video.key}`}
-            style={{ textDecoration: "none", color: "white" }}
+    <>
+      <Box px={0} mb={4} sx={{ display: "flex", alignItems: "center" }}>
+        <FormControl
+          sx={{
+            m: 0,
+            minWidth: 160,
+            border: "1px solid transparent",
+            backgroundColor: "rgba(255,255,255,0.07)",
+          }}
+          size="small"
+        >
+          <Select
+            id="filter"
+            value={filter}
+            onChange={handleChange}
+            sx={{ fontSize: "12px" }}
+            p={0}
+            m={0}
           >
-            <Box
-              sx={{
-                height: "0",
-                paddingBottom: "56.25%",
-                backgroundColor: "rgba(255,255,255,0.07)",
-                position: "relative",
-              }}
+            <StyledMenuItem value="all" selected>
+              All
+            </StyledMenuItem>
+            <StyledMenuItem value="featurette">Featurette</StyledMenuItem>
+            <StyledMenuItem value="behind the scenes">
+              Behind the scenes
+            </StyledMenuItem>
+            <StyledMenuItem value="trailer">Trailer</StyledMenuItem>
+            <StyledMenuItem value="teaser">Teaser</StyledMenuItem>
+          </Select>
+        </FormControl>
+        <Typography
+          variant="span"
+          sx={{ color: "#80868b", fontSize: "14px", fontWeight: "500" }}
+          ml={1}
+        >
+          {videoCount} Videos
+        </Typography>
+      </Box>
+      <Grid
+        pr={6.5}
+        container
+        mb={6}
+        columnSpacing={2}
+        rowSpacing={5}
+        sx={{ backgroundColor: "transparent" }}
+      >
+        {filteredVideos?.map((video) => (
+          <Grid item xs={4}>
+            <Link
+              to={`https://youtube.com/watch?v=${video.key}`}
+              style={{ textDecoration: "none", color: "white" }}
             >
-              <Chip
-                label="1:20"
+              <Box
                 sx={{
-                  borderRadius: "0",
-                  backgroundColor: "black",
-                  padding: "4px 0px",
-                  position: "absolute",
-                  bottom: "0",
-                  right: "0",
-                }}
-              />
-              <IconButton
-                sx={{
-                  position: "absolute",
-                  top: "50%",
-                  left: "50%",
-                  transform: "translate(-50%,-50%)",
-                  padding: 0,
+                  height: "0",
+                  paddingBottom: "56.25%",
+                  backgroundColor: "rgba(255,255,255,0.07)",
+                  position: "relative",
                 }}
               >
-                <PlayCircleFilledWhiteOutlinedIcon sx={{ fontSize: 50 }} />
-              </IconButton>
-            </Box>
+                <Chip
+                  label="1:20"
+                  sx={{
+                    borderRadius: "0",
+                    backgroundColor: "black",
+                    padding: "4px 0px",
+                    position: "absolute",
+                    bottom: "0",
+                    right: "0",
+                  }}
+                />
+                <IconButton
+                  sx={{
+                    position: "absolute",
+                    top: "50%",
+                    left: "50%",
+                    transform: "translate(-50%,-50%)",
+                    padding: 0,
+                  }}
+                >
+                  <PlayCircleFilledWhiteOutlinedIcon sx={{ fontSize: 50 }} />
+                </IconButton>
+              </Box>
 
-            <Typography
-              variant="h2"
-              sx={{
-                fontSize: "16px",
-                fontWeight: "400",
-                letterSpacing: "0.4px",
-              }}
-              mt={1.5}
-              mb={1}
-            >
-              {video.name}
-            </Typography>
-            <Typography
-              variant="p"
-              sx={{
-                fontSize: "14px",
-                fontWeight: "400",
-                letterSpacing: "0.4px",
-                color: "#80868b",
-              }}
-            >
-              {video.type}
-            </Typography>
-          </Link>
-        </Grid>
-      ))}
-    </Grid>
+              <Typography
+                variant="h2"
+                sx={{
+                  fontSize: "16px",
+                  fontWeight: "400",
+                  letterSpacing: "0.4px",
+                }}
+                mt={1.5}
+                mb={1}
+              >
+                {video.name}
+              </Typography>
+              <Typography
+                variant="p"
+                sx={{
+                  fontSize: "14px",
+                  fontWeight: "400",
+                  letterSpacing: "0.4px",
+                  color: "#80868b",
+                }}
+              >
+                {video.type}
+              </Typography>
+            </Link>
+          </Grid>
+        ))}
+      </Grid>
+    </>
   );
 }
