0069778093d28de54e99844e5af11e51a858ed63
Mikaeil Eghbal <mikaeileghbal@yahoo.com>
2023-04-20 11:38:29 +0430

    Add redux default state

---
+++ src/store/defualtStore.js
@@ -0,0 +1,26 @@
+export const defaultStore = {
+  featured: {},
+  detail: {},
+  movie: {
+    trending: [],
+    popular: [],
+    top_rated: [],
+    upcoming: [],
+    now_playing: [],
+    cast: [],
+    like: [],
+    videos: [],
+    images: [],
+  },
+  tv: {
+    trending: [],
+    popular: [],
+    top_rated: [],
+    on_the_air: [],
+    airing_today: [],
+    cast: [],
+    like: [],
+    videos: [],
+    images: [],
+  },
+};
