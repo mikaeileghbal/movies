07e653c4f4370ff0690f6c8da86f6bed6086dd23
Mikaeil Eghbal <mikaeileghbal@yahoo.com>
2023-04-30 10:07:39 +0430

    Refactor rootSagas

--- src/store/sagas.js
+++ src/store/sagas.js
@@ -1,4 +1,4 @@
-import { take, put, call, takeEvery } from "redux-saga/effects";
+import { put, call, takeEvery } from "redux-saga/effects";
 import { requestMovies, setMovies } from "../features/movieSlice";
 import { requestTvs, setTvs } from "../features/tvSlice";
 import Axios from "axios";
@@ -12,48 +12,26 @@ let callAPI = async ({ url, method, data }) => {
   });
 };
 
-export function* fetchDataSaga(listName, mediaType) {
-  console.log("url in saga : ", apiEndpoint[mediaType][listName].url);
+export function* fetchDataSaga(action) {
+  const { listName, mediaType } = action.payload;
+
   try {
     let result = yield call(() =>
       callAPI({ url: apiEndpoint[mediaType][listName].url })
     );
 
-    switch (mediaType) {
-      case "movie":
-        console.log("movie saga");
-        return yield put(setMovies({ listName, data: result.data.results }));
-      case "tv":
-        console.log("tv saga");
-        return yield put(setTvs({ listName, data: result.data.results }));
-      default:
-        return;
+    if (mediaType === "movie")
+      yield put(setMovies({ listName, data: result.data.results }));
+    else {
+      yield put(setTvs({ listName, data: result.data.results }));
     }
   } catch (e) {}
 }
 
 export function* rootSagaMovie() {
-  yield takeEvery(requestMovies, function* fetchData(action) {
-    console.log("payload in movie saga: ", action.payload);
-
-    const { listName, mediaType } = action.payload;
-
-    const url = apiEndpoint[mediaType][listName].url;
-
-    const { data } = yield Axios({
-      url,
-    });
-
-    yield put(setMovies({ listName, data: data.results }));
-  });
-  //yield call(() => fetchDataSaga(payload.listName, payload.mediaType));
+  yield takeEvery(requestMovies, fetchDataSaga);
 }
 
 export function* rootSagaTv() {
-  while (true) {
-    const { payload } = yield take(requestTvs);
-    console.log("payload in tv saga: ", payload);
-
-    yield call(() => fetchDataSaga(payload.listName, payload.mediaType));
-  }
+  yield takeEvery(requestTvs, fetchDataSaga);
 }
